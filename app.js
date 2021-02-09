ff18cac4ef4f8fa54d6e9a7e0711ccd18f2e1ffd
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-02-09 00:27:12 -0800

    added shop sass and fixed fetch

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>{
-                console.log(json)})
-            .catch(err=>{
-                console.log(err);
-            });
+
+// fetch('https://fakestoreapi.com/products/category/women%20clothing')
+//   .then(response => response.json())
+//   .then(json => console.log(json));
+
+
+  async function getData(){
+    const res = await fetch('https://fakestoreapi.com/products/category/women%20clothing')
+    const result = await res.json(); 
+    console.log(result);
+
+    const product = document.querySelector('.product-info');
+    const list = document.querySelector('.shop-product');
+    product.innerHTML = 
+    `<img src=${result[0].image}>
+    <span>${result[0].title}</span>
+    <span>$${result[0].price}</span>`;
+    list.appendChild(product);
+  }
+
+  getData();
